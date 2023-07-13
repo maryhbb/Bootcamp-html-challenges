32616955fbffefc452c6cc982337a0ecead3b3a3
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-13 12:23:06 +0200

    Implement Tak pizza-analyzer

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,18 +9,45 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
-// define the function calculatePizzaGain here
+function calculatePizzaGain(diameter1, diameter2) {
+  const radius1 = diameter1 / 2;
+  const radius2 = diameter2 / 2;
+
+  const area1 = Math.PI * Math.pow(radius1, 2);
+  const area2 = Math.PI * Math.pow(radius2, 2);
+
+  const pizzaGain = ((area2 - area1) / area1) * 100;
+  const roundedPizzaGain = Math.round(pizzaGain);
+
+  output.textContent = roundedPizzaGain;
+}
 
 // Task 2
-// define the function updatePizzaDisplay here
+function updatePizzaDisplay(pizzaElement, newSize) {
+  const newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = `${newDisplaySize}px`;
+}
 
 // Task 3
-// define the function updateOutputColor here
+function updateOutputColor(size1, size2) {
+  outputSection.style.backgroundColor =
+    size2 < size1 ? "var(--red)" : "var(--green)";
+}
