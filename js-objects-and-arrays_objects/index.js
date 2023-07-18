700bb84f927dd9af4b20bdd04a7d04839997d8a2
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-18 20:53:45 +0200

    Implement objects

--- js-objects-and-arrays_objects/index.js
+++ js-objects-and-arrays_objects/index.js
@@ -4,7 +4,9 @@ console.clear();
 // Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "joe",
+  age: 31,
+  email: "joe@doe.com",
 };
 
 // EXERCISE 2
@@ -15,8 +17,8 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
@@ -26,6 +28,9 @@ const personToChange = {
   age: 24,
 };
 
+personToChange.name = "Alex";
+personToChange.age = 35;
+
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
 
@@ -33,6 +38,8 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+
+petPluto.age = 5;
 
 export {
   personWithNameAgeEmail,
