9347e8ca90f8a840c8c112df228dc05115f62cf4
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-20 16:07:01 +0200

    Implement Task handle-form-submit

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -3,18 +3,25 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
-//   console.log(event.target);
 
-//   const formElements = event.target.elements;
+  //   console.log(event.target);
 
-//   console.log(formElements.firstName);
-//   console.log(formElements.firstName.value);
+  //   const formElements = event.target.elements;
+  //   console.log(formElements);
 
-  console.log(event.target);
+  //   console.log(formElements.firstName);
+  //   console.log(formElements.firstName.value);
+
+  //   console.log(event.target);
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
   console.log(data);
+
+  const age = data.age;
+  const badness = data.badness;
+
+  console.log(age + badness);
+  console.log(parseInt(age) + parseInt(badness));
 });
