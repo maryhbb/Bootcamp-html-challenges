c23f2591a352f8ca139fa57b32e4485a21c0fab2
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-21 18:06:00 +0200

    Implement Task js-createelement/product-teaser

--- js-createelement_product-teaser/js/index.js
+++ js-createelement_product-teaser/js/index.js
@@ -9,3 +9,43 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const body = document.body;
+
+const secondArticle = document.createElement("article");
+
+secondArticle.innerHTML = `
+  <div class="product__body">
+    <div class="product__text-container">
+      <h2 class="product__name">${name}</h2>
+      <ul class="product__categories">
+        <li class="product__category">${category1}</li>
+        <li class="product__category">${category2}</li>
+        <li class="product__category">${category3}</li>
+      </ul>
+      <p class="product__description">
+      ${description}
+      </p>
+    </div>
+    <div class="product__image-container">
+      <img
+        class="product__image"
+        src=${imageSrc}
+        alt=""
+      />
+    </div>
+  </div>
+  <footer class="product__footer">
+    <span class="product__price">${price}</span>
+    <button type="button" class="product__buy-button" data-js="buy-Button">Buy</button>
+  </footer>
+`;
+
+secondArticle.classList.add("product");
+body.append(secondArticle);
+
+const buyButtonElement = document.querySelector('[data-js="buy-Button"]');
+
+buyButtonElement.addEventListener("click", () => {
+  console.log(`Product Name: ${name}, Price: ${price}`);
+});
