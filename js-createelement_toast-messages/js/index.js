7e5fee0db8cb4fadc360624965e98df3fc69e8f8
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-21 10:29:15 +0200

    Implement Task js-createelement/toast-messages

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -6,6 +6,12 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const toastList = document.createElement("li");
+  toastContainer.append(toastList);
+
+  toastList.classList.add("toast-container__message");
+  // toastList.textContent = "I'm a toast message.";
+  toastList.append("I'm a toast message.");
 });
 
 clearButton.addEventListener("click", () => {
