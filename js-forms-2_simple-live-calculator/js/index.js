5251105226c69e5c121cba8afcd476582e3f192f
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-24 11:12:23 +0200

    Implement Task js-forms/simple-live-calculator

--- js-forms-2_simple-live-calculator/js/index.js
+++ js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,10 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+function calculateResult() {
+  result.textContent = parseInt(firstInput.value) * parseInt(secondInput.value);
+}
+
+firstInput.addEventListener("input", calculateResult);
+secondInput.addEventListener("input", calculateResult);
